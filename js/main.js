553d57aab647cbbb9ff255e8b4d6213dcf437297
S3rg3nt <87121242+S3rg3nt@users.noreply.github.com>
2021-08-04 20:05:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ $(document).ready(() => {
     $('[data-toggle="tooltip"]').tooltip();
   });
 
-  particlesJS.load('particles-js', 'particlesjs.json');
+  particlesJS.load('particles-js', 'particlesjs-config.json');
 });
